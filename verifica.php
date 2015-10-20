d8bd49f9434128ad858dc3675feefea7f141dbfa
gabriella <gabriella@Gabriella>
2015-10-20 13:40:51 -0200

    modificação do cadastro,login

--- verifica.php
+++ verifica.php
@@ -1,13 +1,14 @@
 <?php
 require 'conexao.php';
-include_once 'model/class/Usuario.class.php';
-if(!empty($_POST['Entrar'])&& !empty($_POST['login']) && !empty($_POST['pswd'])){
+require 'model/class/Usuario.class.php';
+if(!empty($_POST['login']) && !empty($_POST['pswd'])){
         $login=$_POST['login'];
         $senha = md5($_POST['pswd']);
         $usuario = new Usuario();
         $usuario->setLogin($login);
         $usuario->setSenha($senha);
-        if(VerificaUsuario($msqli)){
+        $val=VerificaUsuario($msqli);
+        if($val){
             header('Location: entrar.php');
         }else{
             echo "Login ou senha incorretos";
